10c496f918d3994f5b42b9b39560a6af5395aed1
Max <kosachmax@gmail.com>
2023-07-12 22:55:54 +0000

    Trying to pass through first review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,15 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['158.160.70.31', '127.0.0.1', 'localhost', 'taskosach.webhop.me']
 
 # Application definition
 
@@ -103,8 +104,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+# Замените стандартное значение 'static' на 'static_backend',
+# чтобы не было конфликта запросов к статике фронтенда и бэкенда.
+STATIC_URL = 'static_backend/'
+# Укажите директорию, куда бэкенд-приложение должно сложить статику.
+STATIC_ROOT = BASE_DIR / 'static_backend/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 
 from api import views
 
+
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
